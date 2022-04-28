c2442263211cd4b11a430a311236c599acebe35d
giftade <93712143+giftade@users.noreply.github.com>
2022-04-28 10:10:35 +0100

    Add files via upload

---
+++ digital clock.js
@@ -0,0 +1,40 @@
+function gettime()
+{
+    var hour=new Date().getHours();
+    var min=new Date().getMinutes();
+    var sec=new Date().getSeconds();
+
+    if (min < 10) 
+    {
+        min= "0" + min;
+}
+
+if (sec < 10) 
+{
+    sec= "0" + sec;
+}
+
+
+if (hour < 10) 
+{
+    hour= "0" + hour;
+}
+// AM & PM
+if (hour>= 12) 
+{
+    period= "PM"
+} else {
+    period= "AM"
+}
+
+document.form.clock.value= hour+" : "+min+" : "+sec+" "+period;
+setTimeout("gettime()",100)
+
+
+
+
+
+
+
+
+}
